5958018c6e6ee297abb45b0c1282d02a9d9d5049
rookie1024 <rookie1286@gmail.com>
2019-01-09 09:56:12 -0800

    Added a history-wiping function.

---
+++ parts/80-functions.d/kgb.zsh
@@ -0,0 +1,13 @@
+function kgb() {
+  local newfile
+
+  newfile="$HISTFILE.new"
+
+  grep -Fva "${(F)@}" "$HISTFILE" >"$newfile"
+
+  git diff "$HISTFILE" "$newfile"
+
+  if [[ $(_rc_g_yn "Apply changes? [y/N]" n) == 'y' ]]; then
+    mv "$newfile" "$HISTFILE"
+  fi
+}
