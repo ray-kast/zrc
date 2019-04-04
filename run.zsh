777916487d139c2166289c2bb4a48b988db792fd
rookie1024 <rookie1286@gmail.com>
2019-04-04 15:07:26 -0700

    Added an updater.

--- run.zsh
+++ run.zsh
@@ -1,6 +1,6 @@
 if [[ -t 1 ]] then
   function _rc_i_status() {
-    echo -n "\r\x1b[2K[zrc] ${(j: :)@} "
+    echo -n "\r\x1b[2K[zrc] ${(j: :)@}"
   }
 
   function _rc_i_status_reset() {
@@ -16,17 +16,55 @@ fi
 
 _rc_i_basedir="$(dirname "$0")"
 
-for file in "$_rc_i_basedir"/utils/**/*(on); do
-  _rc_i_status util ${${file#$_rc_i_basedir/utils}##/}
-  . $file
-  unfunction -m '_rc_l_*'
-done
+() {
+  local file
 
-for file in "$_rc_i_basedir"/parts/**/*(on); do
-  _rc_i_status util ${${file#$_rc_i_basedir/parts}##/}
-  . $file
-  unfunction -m '_rc_l_*'
-done
+  for file in "$_rc_i_basedir"/utils/**/*(on); do
+    _rc_i_status util ${${file#$_rc_i_basedir/utils}##/}
+    . $file
+    unfunction -m '_rc_l_*'
+  done
+
+  if [[ -t 1 ]]; then
+    () {
+      local file
+
+      file="$ZDOTDIR"/.zrc-update
+
+      [[ -n "$(find "$file" -atime +0 2>/dev/null)" || (! -f "$file") ]] || return
+
+      _rc_i_status "Checking for updates..."
+
+      (
+        cd "$HOME"/.zrc
+
+        git fetch -q || exit 1
+
+        [[ "$(git rev-parse HEAD)" != "$(git rev-parse '@{u}')" ]] || {
+          _rc_i_status_reset
+          echo 'No update available.'
+          exit 0
+        }
+
+        _rc_i_status_reset
+
+        [[ "$(_rc_g_yn "Update ~/.zrc? [Y/n] " y)" == 'y' ]] || exit 0
+
+        git pull
+
+        exit 1
+      ) || { _rc_i_status_reset; echo "WARNING: update check failed"; return }
+
+      touch "$file"
+    }
+  fi
+
+  for file in "$_rc_i_basedir"/parts/**/*(on); do
+    _rc_i_status util ${${file#$_rc_i_basedir/parts}##/}
+    . $file
+    unfunction -m '_rc_l_*'
+  done
+}
 
 if [[ -f ~/.zrc-local.zsh ]]; then
   _rc_i_status zrc-local
