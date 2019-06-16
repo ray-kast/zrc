7213c9d8bda91d22fe3348c4f21fe3b70aeeda1c
rookie1024 <rookie1286@gmail.com>
2019-06-16 14:57:30 -0700

    Make the default locale UTF-8.

--- env.zsh
+++ env.zsh
@@ -3,6 +3,9 @@
 
 # Not exporting .zrc/bin because it's really just interactive utilities
 
+[[ -v LC_ALL ]] || export LC_ALL="" # Yeah, this is redundant, but not if I ever change LC_ALL
+[[ -v LC_CTYPE ]] || export LC_CTYPE="en_US.UTF-8"
+
 # cargo
 export PATH=$PATH:$HOME/.cargo/bin
 
