aad73208090e6f86a649d98a6d97a07f9276ccd7
rookie1024 <rookie1286@gmail.com>
2019-10-11 18:28:42 -0700

    Added virtualenv helpers.

---
+++ parts/80-functions.d/ve.zsh
@@ -0,0 +1,24 @@
+function ve() {
+  local dir
+
+  dir="$1"
+
+  shift
+
+  virtualenv "$HOME/.venv/$dir" $@
+  return $?
+}
+
+function ve-rm() {
+  rm -rf "$HOME/.venv/$1"
+}
+
+function ve-ls() {
+  ls -lahbF --color "$HOME/.venv/$1"
+}
+
+function va() {
+  source "$HOME/.venv/$1/bin/activate"
+}
+
+alias vd='deactivate'
