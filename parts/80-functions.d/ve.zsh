55615abf24e3c177de33da774a6308495c034e9e
rookie1024 <rookie1286@gmail.com>
2020-10-25 11:08:22 -0700

    Fixed ve to attempt to use venv.

--- parts/80-functions.d/ve.zsh
+++ parts/80-functions.d/ve.zsh
@@ -1,11 +1,27 @@
 function ve() {
-  local dir
+  local dir python pyver venv
 
   dir="$1"
-
   shift || return 1
 
-  virtualenv "$HOME/.venv/$dir" $@
+  python="$1"
+  shift || python="python"
+  python="$(which "$python")"
+
+  pyver="$({ "$python" --version 2>&1 } | awk -F' ' '/python/i { print $2 }')"
+
+  echo "\x1b[1m==> Python version detected: $pyver\x1b[m"
+
+  if { "$python" -c 'import venv' >/dev/null 2>/dev/null }; then
+    echo "\x1b[1m==> Using venv module...\x1b[m"
+    venv=("$python" -m venv)
+  else
+    echo "\x1b[1;38;5;3m=!> Using virtualenv...\x1b[m"
+    venv=("$(which virtualenv)" -p "$python")
+  fi
+
+  "${(@)venv}" "$HOME/.venv/$dir"
+
   return $?
 }
 
