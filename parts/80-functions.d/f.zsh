195aebb8a159443137993c41d316b5335e464a86
rookie1024 <rookie1286@gmail.com>
2021-01-20 17:39:49 -0800

    Fixed behavior of f with directories.

--- parts/80-functions.d/f.zsh
+++ parts/80-functions.d/f.zsh
@@ -9,9 +9,17 @@ function f() {
   (( # > 0 )) && popd >/dev/null 2>/dev/null
 
   text=''
-  [[ "$(file "$fname")" =~ 'ASCII text' ]] && text='y'
+  [[ "$(file "$fname")" =~ '(ASCII|Unicode) text' ]] && text='y'
 
-  if [[ -x "$fname" ]]; then
+  if [[ -d "$fname" ]]; then
+    ty='directory'
+  else
+    ty='file'
+  fi
+
+  file "$fname"
+
+  if [[ ! -d "$fname" && -x "$fname" ]]; then
     if [[ "$(_rc_g_yn 'Run executable? [y/N] ' n)" == 'y' ]]; then
       (exec "$(realpath "$fname")")
     elif [[ -n "$text" ]]; then
@@ -20,10 +28,12 @@ function f() {
   else
     if [[ -n "$text" ]]; then
       nvim "$fname"
-    elif _rc_g_has open; then
-      open "$fname"
-    else
-      gio open "$fname"
+    elif [[ "$(_rc_g_yn "Open $ty? [Y/n] " y)" == 'y' ]]; then
+      if _rc_g_has open; then
+        open "$fname"
+      else
+        gio open "$fname"
+      fi
     fi
   fi
 }
