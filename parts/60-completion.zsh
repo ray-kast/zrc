3f0e05c1dbf330ed59bc4486557391ca645928ea
raykast <rookie1286@gmail.com>
2021-08-30 14:46:59 -0700

    Was finally forced to rewrite completion settings.

--- parts/60-completion.zsh
+++ parts/60-completion.zsh
@@ -48,33 +48,54 @@ function _rc_l_scroll_prompt() {
 
 fpath=($HOME/.zrc/completion $fpath)
 
+zstyle ':completion:*' prompt "$(_rc_l_corr_prompt)" # ???
+
+
+zstyle ':completion:*' accept-exact false # Interferes with . for filenames
+zstyle ':completion:*' accept-exact-dirs false
+zstyle ':completion:*' add-space true
 zstyle ':completion:*' ambiguous true
 zstyle ':completion:*' auto-description 'specify: %d'
 zstyle ':completion:*' completer \
+  _extensions \
   _expand \
   _complete \
+  _match \
+  _history \
   _ignored \
   _correct \
-  _approximate
-
-zstyle ':completion:*' completions 1
+  _approximate \
+  _prefix
 zstyle ':completion:*' expand prefix suffix
+zstyle ':completion:*' file-list list
 zstyle ':completion:*' file-sort name
 zstyle ':completion:*' format "$(_rc_l_compl_prompt)"
-zstyle ':completion:*' glob 'NUMERIC == 1'
+zstyle ':completion:*' glob true
+zstyle ':completion:*' global true
 zstyle ':completion:*' group-name ''
+zstyle ':completion:*' group-order local-directories commands builtins functions
 zstyle ':completion:*' ignore-parents parent pwd ..
 zstyle ':completion:*' insert-unambiguous true
-zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-dirs-first true
+zstyle ':completion:*' list-grouped true
 zstyle ':completion:*' list-prompt "$(_rc_l_list_prompt)"
-zstyle ':completion:*' list-suffixes true
-zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:lower:][:upper:]}={[:upper:][:lower:]} r:|[._-]=** r:|=**' '+l:|=* r:|=*'
-zstyle ':completion:*' max-errors 3 numeric
-zstyle ':completion:*' menu select=2
+zstyle ':completion:*' list-separator '>>'
+zstyle ':completion:*' match-original true
+zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} r:|[.]=** e:=*' '+b:=*'
+zstyle ':completion:*' max-errors 4 numeric
+zstyle ':completion:*' menu select=3
 zstyle ':completion:*' original true
-zstyle ':completion:*' prompt "$(_rc_l_corr_prompt)"
+zstyle ':completion:*' path-completion true
+zstyle ':completion:*' prefix-hidden false
+zstyle ':completion:*' range 1000:10 # for history words, try 1000 words 10 at a time
+zstyle ':completion:*' remove-all-dups true # also for history words
 zstyle ':completion:*' select-prompt "$(_rc_l_scroll_prompt)"
-zstyle ':completion:*' substitute 'NUMERIC == 2'
+zstyle ':completion:*' select-scroll 0
+zstyle ':completion:*' show-ambiguity true
+# zstyle ':completion:*' show-completer true
+zstyle ':completion:*' substitute true
+zstyle ':completion:*' suffix true
 zstyle ':completion:*' verbose true
 
 autoload -Uz compinit
@@ -82,5 +103,6 @@ autoload -Uz compinit
 export ZCOMPDUMP=$ZDOTDIR/.zcompdump
 
 compinit -d $ZCOMPDUMP
+_comp_options+=(globdots)
 
 _rc_g_has kitty && kitty + complete setup zsh | source /dev/stdin
