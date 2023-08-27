8f403925046cf4d033cdfdd91638ca9f0930e7ba
raykast <me@ryan-s.net>
2023-08-27 15:12:16 -0700

    Fix issue with bailing out of fzf functions.

--- parts/80-functions.d/fzf.zsh
+++ parts/80-functions.d/fzf.zsh
@@ -1,13 +1,15 @@
 function fcd() {
-  cd "$(command f -td -d"${1:-.}")"
+  local dir
+  dir="$(command f -td -d"${1:-.}")" && cd "$dir"
 }
 
 alias fhc='fcd ~'
 
 function fnv() {
-  local cmd="cd ${(D)$(command f -td -d"${1:-$HOME}")}" esc="$cmd"
+  local cmd esc
+  cmd="cd ${(D)$(command f -td -d"${1:-$HOME}")}" || return "$?"
 
-  [[ "${(q)cmd}" == "$cmd" ]] || esc="${(qq)cmd}"
+  [[ "${(q)cmd}" == "$cmd" ]] && esc="$cmd" || esc="${(qq)cmd}"
 
   nvim -c "$cmd" && print -s nvim -c "$esc"
 }
