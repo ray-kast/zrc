79433ca0b3cff70bf7ad6618b68c302f6bb4c07c
raykast <me@ryan-s.net>
2023-08-27 16:17:07 -0700

    Improve implementation of cfg-install functions.

--- parts/80-functions.d/config.zsh
+++ parts/80-functions.d/config.zsh
@@ -1,15 +1,13 @@
 {
   typeset -a cmd=(
-    local dir='"${XDG_CONFIG_DIR:-$HOME/.config}/nvim"'\;
+    local base='"$1"' dir\;
+    dir='"${XDG_CONFIG_DIR:-$HOME/.config}/$base"'\;
+    '((' \# \> 0 '))' '&&' shift '||' return 1\;
     mkdir -vp '"$dir"' '&&'
-    ln -ibsvt '"$dir"' "'$_rc_i_basedir/config/nvim/init.lua'"
+    ln -ibsvt '"$dir"' "'$_rc_i_basedir/config/'"'"$base/${(@)^@}"'
   )
-  functions[cfg-install-nvim]=$cmd
-
-  cmd=(
-    local dir='"${XDG_CONFIG_DIR:-$HOME/.config}/ranger"'\;
-    mkdir -vp '"$dir"' '&&'
-    ln -ibsvt '"$dir"' "'$_rc_i_basedir/config/ranger/'{rc.conf,rifle.conf,scope.sh}"
-  )
-  functions[cfg-install-ranger]=$cmd
+  functions[_rc_g_cfg_install]=$cmd
 }
+
+alias cfg-install-nvim='_rc_g_cfg_install nvim init.lua'
+alias cfg-install-ranger='_rc_g_cfg_install ranger rc.conf rifle.conf scope.sh'
