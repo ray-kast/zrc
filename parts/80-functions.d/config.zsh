ab90efd53f22b2f6d778489e31e009711469ca69
raykast <me@ryan-s.net>
2023-08-26 23:17:44 -0700

    Fix logic error in cfg-install-nvim.

--- parts/80-functions.d/config.zsh
+++ parts/80-functions.d/config.zsh
@@ -1,4 +1,8 @@
 {
-  typeset -a cmd=(ln -ibsvt '"${XDG_CONFIG_DIR:-$HOME/.config}/nvim"' "'$_rc_i_basedir/config/nvim/init.lua'")
+  typeset -a cmd=(
+		local dir='"${XDG_CONFIG_DIR:-$HOME/.config}/nvim"'\;
+		mkdir -vp '"$dir"' '&&'
+		ln -ibsvt '"$dir"' "'$_rc_i_basedir/config/nvim/init.lua'"
+	)
   functions[cfg-install-nvim]=$cmd
 }
