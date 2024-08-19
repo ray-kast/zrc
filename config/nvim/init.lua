c2b58438596b9d7ccecba81b43dd735f30ecfd6d
raykast <me@june-s.net>
2024-08-19 10:51:00 -0700

    Add diffchar.vim and update Git aliases.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -797,39 +797,9 @@ if not vim.g.lazy_did_setup then
         vim.notify = require'notify'
       end
     },
-    -- {
-    --   -- 'phaazon/hop.nvim', -- TODO: ded
-    --   'smoka7/hop.nvim',
-    --   config = function(_, opts)
-    --     local hop = require'hop'
-    --     local directions = require'hop.hint'.HintDirection
-    --     hop.setup(opts)
-
-    --     vim.keymap.set('', 'f', function()
-    --       hop.hint_char1{ direction = directions.AFTER_CURSOR, current_line_only = false }
-    --     end, { remap = true })
-
-    --     vim.keymap.set('', 'F', function()
-    --       hop.hint_char1{ direction = directions.BEFORE_CURSOR, current_line_only = false }
-    --     end, { remap = true })
-
-    --     vim.keymap.set('', 't', function()
-    --       hop.hint_char1{
-    --         direction = directions.AFTER_CURSOR,
-    --         current_line_only = false,
-    --         hint_offset = -1,
-    --       }
-    --     end, { remap = true })
-
-    --     vim.keymap.set('', 'T', function()
-    --       hop.hint_char1{
-    --         direction = directions.BEFORE_CURSOR,
-    --         current_line_only = false,
-    --         hint_offset = 1,
-    --       }
-    --     end, { remap = true })
-    --   end,
-    -- },
+    {
+      'rickhowe/diffchar.vim',
+    },
     {
       'rmagatti/auto-session',
       opts = {
